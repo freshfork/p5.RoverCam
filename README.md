c33d225ec864cd69eb13548ae7b40e2e5f130171
James Dunn <jwdunn1@gmail.com>
2020-07-03 02:00:59 -0700

    v1.1.1
    Add pointerLock per issue #4 and additional enhancements.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## p5.RoverCam
 A super-simple first-person perspective camera library for p5.js in WEBGL mode.
 
-The position and orientation of an instance of RoverCam can be controlled using the mouse and WASD / Arrow keys (as well as E and Q for upward and downward motion).
+RoverCam supports p5.js global and multi-instance modes.
 
-Source forked from github.com/jrc03c/queasycam and ported to JavaScript.
+The position and orientation of an instance of RoverCam can be controlled using the mouse and WASD / Arrow keys (as well as E and Q for upward and downward motion).
 
 ### Usage
 
@@ -12,9 +12,14 @@ var rover;
 
 function setup() {
   createCanvas(800, 800, WEBGL);
-  rover = new RoverCam(this);
-  rover.speed = 5;              // optional; default is 3
-  rover.sensitivity = 0.5;      // optional; default is 2
+  rover = new RoverCam();
+  rover.usePointerLock();    // optional; default is keyboard control only
+  rover.setState({           // optional
+    position: [-400,-200,-200],
+    rotation: [0.4,0.3,0],
+    sensitivity: 0.1,
+    speed: 0.5
+  });
 }
 
 function draw() {
@@ -23,23 +28,81 @@ function draw() {
 }
 ```
 
-After creating the camera, it can be controlled using the mouse or the keyboard with the following:
+After creating the camera, it can be controlled using the mouse or the keyboard with the following default controls:
 
 ```
-Mouse:
-       left/right : pan
-       up/down : tilt
-       click : move forward
+Mouse: click : toggle pointer lock
+       left/right : yaw
+       up/down : pitch
 
- Keys: a/d : left/right
+ Keys: a/d : yaw | left/right*
        w/s : forward/backward
        e/q : up/down
+       r/f : pitch | elevation*
        ←/→ : left/right
        ↑/↓ : forward/backward
+       +/- : field of view
 
+* when pointerLock is enabled
 ```
 
-RoverCam can be extended:
+
+### Examples
+
+- [MazeRunner on openprocessing](https://www.openprocessing.org/sketch/755273)
+- [MazeRunner on editor.p5js.org](https://editor.p5js.org/jwdunn1/sketches/iI-2XX0Hw)
+- [Multi-camera](https://editor.p5js.org/jwdunn1/sketches/L6xSzdKM8)
+- [Multi-instance](https://editor.p5js.org/jwdunn1/sketches/QzBAQZPVT)
+- [Stereo vision](https://editor.p5js.org/jwdunn1/sketches/nxfEMXn-s)
+- [Controller](https://editor.p5js.org/jwdunn1/sketches/3IaJbTnkd)
+
+### Utility methods
+
+Mouse pointer lock can be enabled by invoking the `usePointerLock()` method. Pointer lock is global for all instances, thus only one instance needs to set this. See the multi-instance and stereo vision examples above.
+
+```javascript
+rover.usePointerLock();
+
+// if using p5.js instance mode, pass the instance variable
+rover.usePointerLock(p);
+```
+
+The camera can be reset to initialization values with the `reset()` method.
+
+```javascript
+rover.reset();
+```
+
+Camera state can be set with the `setState()` method. An object argument may contain any of the following optional property/value pairs:
+
+```javascript
+rover.setState = {
+  active: true,
+  enableControl: false,
+  position: [-400,50,0],
+  rotation: [0,0.1,0],
+  offset: [0,20],
+  fov: 1,
+  speed: 0.1,
+  sensitivity: 0.02
+};
+```
+
+Camera controls can be enabled or disabled by setting the boolean `enableControl` property. This is useful to switch camera controls in a multi-instance scenario. The camera view remains visible. See the multi-instance example above. By default, a camera's controls are enabled.
+
+```javascript
+rover.enableControl = true;
+```
+
+A camera view can be enabled or disabled with the `setActive()` method. It requires one boolean argument. When disabled, the camera is no longer updated or visible. Another view can take its place. See the multi-camera example above. By default, a camera's view is enabled.
+
+```javascript
+rover.setActive(true);
+```
+
+### Customization
+
+The RoverCam class can be extended. See the MazeRunner example above.
 
 ```javascript
 class Player extends RoverCam {
@@ -48,7 +111,7 @@ class Player extends RoverCam {
     this.speed = 0.04;
     this.dimensions = createVector(1, 3, 1);
     this.velocity = createVector(0, 0, 0);
-    this.gravity = createVector(0, 0.015, 0);
+    this.gravity = createVector(0, 0.03, 0);
     this.grounded = false;
   }
   
@@ -57,7 +120,7 @@ class Player extends RoverCam {
     this.position.add(this.velocity);
     
     // extend the keyboard controls by adding a hop behavior
-    if (this.grounded && keyIsPressed && keyCode == 32){ // space
+    if (this.grounded && keyIsDown(32)){ // space
       this.grounded = false;
       this.velocity.y = -1.5;
       this.position.y -= 0.2;
@@ -66,6 +129,65 @@ class Player extends RoverCam {
 }
 ```
 
-Example:
-- [MazeRunner on openprocessing](https://www.openprocessing.org/sketch/755273)
-- [MazeRunner on editor.p5js.org](https://editor.p5js.org/jwdunn1/sketches/Xst_YZq1V)
+The keyboard can be remapped. The following example replaces the `w,a,s,d` primary movement keys to `k,j,l,h`
+
+```javascript
+var rover;
+
+function setup() {
+  createCanvas(800, 800, WEBGL);
+  rover = new RoverCam();
+  rover.keyMap.mx1 = [75, 38];  // k, UP_ARROW
+  rover.keyMap.mx2 = [74, 40];  // j, DOWN_ARROW
+  rover.keyMap.my1 = [72, 37];  // h, LEFT_ARROW
+  rover.keyMap.my2 = [76, 39];  // l, RIGHT_ARROW
+}
+```
+
+
+The controller method can be customized. It must call on the primitive translation and rotation methods to move the camera. See the controller example above.
+
+```javascript
+
+  rover.controller = function() { // override
+    if (RoverCam.pointerLock) {
+      this.yaw(movedX * this.sensitivity / 10);   // mouse left/right
+      this.pitch(movedY * this.sensitivity / 10); // mouse up/down
+      if(keyIsDown(72) || keyIsDown(LEFT_ARROW))  this.moveY(this.speed); // h
+      if(keyIsDown(76) || keyIsDown(RIGHT_ARROW)) this.moveY(-this.speed);// l
+    }
+    else { // otherwise yaw/pitch with keys
+      if (keyIsDown(72) || keyIsDown(LEFT_ARROW)) this.yaw(-0.02); // h
+      if (keyIsDown(76) || keyIsDown(RIGHT_ARROW)) this.yaw(0.02); // l
+      if (keyIsDown(82)) this.pitch(-0.02); // r
+      if (keyIsDown(70)) this.pitch(0.02);  // f
+    }
+    if (keyIsDown(75) || keyIsDown(UP_ARROW)) this.moveX(this.speed);    // k
+    if (keyIsDown(74) || keyIsDown(DOWN_ARROW)) this.moveX(-this.speed); // j
+    if (keyIsDown(69)) this.moveZ(this.speed); // e
+  };
+```
+### Primitive internal camera control methods
+
+The following methods control the camera translation and rotation. The camera local axes are X in front/behind, Y to the right/left, and Z above/below.
+
+#### Translation
+
+- `moveX()` translates the camera forward or backward
+- `moveY()` translates the camera left or right
+- `moveZ()` translates the camera up or down
+- `elevate()` adjusts the camera's up/down position by an offset
+
+#### Rotation
+
+- `yaw()` rotates the camera left or right (pan)
+- `pitch()` rotates the camera up or right (tilt)
+- `roll()` rotates the camera along its X axis (TBD)
+
+#### Field of view
+
+- `fov()` adjusts the camera's fovy perspective value
+
+### History
+
+Source forked from github.com/jrc03c/queasycam and ported to JavaScript.
--- p5.rovercam.js
+++ p5.rovercam.js
@@ -14,76 +14,205 @@
  * p5.RoverCam is a derivative of the QueasyCam Library by Josh Castle,
  * ported to JavaScript for p5.js from github.com/jrc03c/queasycam
  *
+ * updates
+ * 20200628 incorporate pointerLock and overridable controller method
+ * 20200629 add support for switching between multiple cameras
+ * 20200701 v1.1.0 fix registerMethod and allow for p5js instance mode
+ * 20200702 v1.1.1 moved pointerLock; added keymap and occular offsetting
  */
 
 // First-person camera control
 // Mouse:
-//       left/right : pan
-//       up/down : tilt
-//       click : move forward
+//       left/right : yaw
+//       up/down : pitch
+//       click : enter/leave pointerLock
 
-// Keys: a/d : left/right
+// Keys: a/d : yaw or left/right if pointerLock
 //       w/s : forward/backward
 //       e/q : up/down
 
-// First, define a callback system
-// a location to store the class instances
-var __RoverCam_cbq = [];
-
-// let p5 know that we want a callback at the end of the draw loop.
-// we iteratively call each item in the queue with its own context
-p5.prototype.registerMethod('post', ()=>{for(let i of __RoverCam_cbq)i.draw.call(i)});
-
 class RoverCam {
-  constructor(){
-    this.speed = 3.0;
-    this.sensitivity = 2.0;
-    this.position = createVector(0, 0, 0);
-    this.velocity = createVector(0, 0, 0);
-    this.up = createVector(0, 1, 0);
-    this.right = createVector(1, 0, 0);
-    this.forward = createVector(0, 0, 1);
+  constructor(instance) {
+    this.sensitivity = 0.02;
+    this.friction = 0.8;
+    this.speed = 0.1;
+    this.reset();
+    this.active = true; // use the setActive method
+    this.enableControl = true; // used to enable/disable controls
+    if(instance !== undefined) this.p5 = instance; 
+    else this.p5 = p5.instance;
+    if(this.p5 !== null) 
+      this.p5.registerMethod('post', () => {if (this.active) this.draw();});
+    this.keyMap = {   // maps each control command to a pair of keys
+      mx1: [87, 38],  // w, UP_ARROW
+      mx2: [83, 40],  // s, DOWN_ARROW
+      my1: [65, 37],  // a, LEFT_ARROW
+      my2: [68, 39],  // d, RIGHT_ARROW
+      mz1: [69, 69],  // e
+      mz2: [81, 81],  // q
+      y1:  [65, 37],  // a, LEFT_ARROW
+      y2:  [68, 39],  // d, RIGHT_ARROW
+      p1:  [82, 82],  // r
+      p2:  [70, 70],  // f
+      r1:  [90, 90],  // z
+      r2:  [67, 67],  // c
+      f1:  [107,187], // +
+      f2:  [109,189], // -
+      e1:  [82, 82],  // r
+      e2:  [70, 70]   // f
+    };
+  }
+
+  // Application can override the following method
+  controller() { // default behavior
+    if (!this.enableControl) return;
+    var k = this.keyMap, p = this.p5;
+    if (RoverCam.pointerLock) {
+      this.yaw(p.movedX * this.sensitivity / 10); // mouse left/right
+      this.pitch(p.movedY * this.sensitivity / 10); // mouse up/down
+      if (p.keyIsDown(k.my1[0]) || p.keyIsDown(k.my1[1])) this.moveY( this.speed); // a
+      if (p.keyIsDown(k.my2[0]) || p.keyIsDown(k.my2[1])) this.moveY(-this.speed); // d
+      if (p.keyIsDown(k.e1[0]) || p.keyIsDown(k.e1[1])) this.elevate(-this.speed); // r
+      if (p.keyIsDown(k.e2[0]) || p.keyIsDown(k.e2[1])) this.elevate(this.speed); // f
+    } else { // otherwise yaw/pitch with keys
+      if (p.keyIsDown(k.y1[0]) || p.keyIsDown(k.y1[1])) this.yaw(-this.sensitivity); // a
+      if (p.keyIsDown(k.y2[0]) || p.keyIsDown(k.y2[1])) this.yaw(this.sensitivity); // d
+      if (p.keyIsDown(k.p1[0]) || p.keyIsDown(k.p1[1])) this.pitch(-this.sensitivity); // r
+      if (p.keyIsDown(k.p2[0]) || p.keyIsDown(k.p2[1])) this.pitch(this.sensitivity); // f
+    }
+    if (p.keyIsDown(k.mx1[0]) || p.keyIsDown(k.mx1[1])) this.moveX(this.speed); // w
+    if (p.keyIsDown(k.mx2[0]) || p.keyIsDown(k.mx2[1])) this.moveX(-this.speed); // s
+    if (p.keyIsDown(k.mz1[0]) || p.keyIsDown(k.mz1[1])) this.moveZ(this.speed); // e
+    if (p.keyIsDown(k.mz2[0]) || p.keyIsDown(k.mz2[1])) this.moveZ(-this.speed); // q
+
+    if (p.keyIsDown(k.f1[0]) || p.keyIsDown(k.f1[1])) this.fov(-this.sensitivity / 10); // +
+    if (p.keyIsDown(k.f2[0]) || p.keyIsDown(k.f2[1])) this.fov(this.sensitivity / 10); // -
+
+    // test roll TBD
+    //if(p.keyIsDown(k.r1[0]) || p.keyIsDown(k.r1[1])) this.roll(this.sensitivity);  // z
+    //if(p.keyIsDown(k.r2[0]) || p.keyIsDown(k.r2[1])) this.roll(-this.sensitivity); // c
+  }
+
+  // Primitive internal camera control methods
+  moveX(speed) {
+    this.velocity.add(p5.Vector.mult(this.forward, speed));
+  }
+  moveY(speed) {
+    this.velocity.add(p5.Vector.mult(this.right, speed));
+  }
+  moveZ(speed) {
+    this.velocity.add(p5.Vector.mult(this.up, -speed));
+  }
+  yaw(angle) {
+    this.pan += angle;
+  }
+  pitch(angle) {
+    this.tilt += angle;
+    this.tilt = this.clamp(this.tilt, -Math.PI / 2.01, Math.PI / 2.01);
+    if (this.tilt == Math.PI / 2.0) this.tilt += 0.001;
+  }
+  roll(angle) { // TBD: useful for flight sim or sloped racetracks
+    this.rot += angle;
+  }
+  fov(angle) {
+    this.fovy += angle;
+    this.width = 0; // trigger a perspective call in the draw loop
+  }
+  elevate(delta) {
+    this.offset[0] += delta;
+  }
+
+  // Utility methods
+  usePointerLock(instance) {
+    if(instance === undefined) instance = p5.instance;
+    if(instance === null) return;
+    RoverCam.canvas = instance._renderer.elt;
+    // ffd8 - click into pointerlock example based on:
+    // https://p5js.org/reference/#/p5/exitPointerLock
+    document.addEventListener('click', () => {
+      if (!RoverCam.pointerLock) {
+        RoverCam.pointerLock = true;
+        instance.requestPointerLock();
+      } else {
+        instance.exitPointerLock();
+        RoverCam.pointerLock = false;
+      }
+    }, false);
+    document.addEventListener('pointerlockchange', RoverCam.onPointerlockChange, false);
+  }
+  reset() {
     this.pan = 0.0;
     this.tilt = 0.0;
-    this.friction = 0.75;
-    this.width = 0;
+    this.rot = 0.0;
+    this.fovy = 1.0;
+    this.width = 0; // trigger a perspective call in the draw loop
     this.height = 0;
-    // push 'this' onto a callback queue
-    __RoverCam_cbq.push(this);
+    this.position = new p5.Vector(0, 0, 0);
+    this.velocity = new p5.Vector(0, 0, 0);
+    this.up = new p5.Vector(0, 1, 0);
+    this.right = new p5.Vector(1, 0, 0);
+    this.forward = new p5.Vector(0, 0, 1);
+    this.offset = [0,0]; // ffd8 - adjust height of cam
   }
-  draw(){
-    if(width !== this.width || height !== this.height){
-        perspective(PI/3, width/height, 0.01, 1000.0);
-        this.width = width;
-        this.height = height;
+  setActive(active){ // method to switch between multiple cameras
+    this.active = active;
+    if(active) this.width=0; // trigger a perspective call in the draw loop
+  }
+  setState(state){ // state object can have fov,active,rotation,position
+    if(state.fov !== undefined) {
+      this.fovy = state.fov;
+      this.width = 0; // trigger a perspective call in the draw loop;
+    }
+    if(state.active !== undefined) this.active = state.active;
+    if(state.rotation !== undefined) {
+      this.pan = state.rotation[0];
+      this.tilt = state.rotation[1];
+      this.rot = state.rotation[2];
+    }
+    if(state.position !== undefined) this.position = new p5.Vector(state.position[0],state.position[1],state.position[2]);
+    if(state.offset !== undefined) this.offset = state.offset;
+    if(state.enableControl !== undefined) this.enableControl = state.enableControl;
+    if(state.speed !== undefined) this.speed = state.speed;
+    if(state.sensitivity !== undefined) this.sensitivity = state.sensitivity;
+  }
+
+  // This method is called after the main p5.js draw loop 
+  draw() {
+    if (this.p5.width !== this.width || this.p5.height !== this.height) {
+      this.p5.perspective(this.fovy, this.p5.width / this.p5.height, 0.01, 10000.0);
+      this.width = this.p5.width;
+      this.height = this.p5.height;
     }
 
-    this.pan += map(mouseX - pmouseX, 0, width, 0, TWO_PI) * this.sensitivity;
-    this.tilt += map(mouseY - pmouseY, 0, height, 0, PI) * this.sensitivity;
-    this.tilt = this.clamp(this.tilt, -PI/2.01, PI/2.01);
+    // Call the potentially overridden controller method
+    this.controller();
 
-    if (this.tilt == PI/2.0) this.tilt += 0.001;
-
-    this.forward = createVector(cos(this.pan), tan(this.tilt), sin(this.pan));
+    this.forward = new p5.Vector(Math.cos(this.pan), Math.tan(this.tilt), Math.sin(this.pan));
     this.forward.normalize();
-    this.right = createVector(cos(this.pan - PI/2.0), 0, sin(this.pan - PI/2.0));
-    if(keyIsDown(65) || keyIsDown(LEFT_ARROW)) this.velocity.add(p5.Vector.mult(this.right, this.speed)); // a
-    if(keyIsDown(68) || keyIsDown(RIGHT_ARROW)) this.velocity.sub(p5.Vector.mult(this.right, this.speed)); // d
-    if(keyIsDown(87) || keyIsDown(UP_ARROW)) this.velocity.add(p5.Vector.mult(this.forward, this.speed)); // w
-    if(keyIsDown(83) || keyIsDown(DOWN_ARROW)) this.velocity.sub(p5.Vector.mult(this.forward, this.speed)); // s
-    if(keyIsDown(81)) this.velocity.add(p5.Vector.mult(this.up, this.speed)); // q
-    if(keyIsDown(69)) this.velocity.sub(p5.Vector.mult(this.up, this.speed)); // e
-    if(mouseIsPressed) this.velocity.add(p5.Vector.mult(this.forward, this.speed));
+    this.right = new p5.Vector(Math.cos(this.pan - Math.PI / 2.0), 0, Math.sin(this.pan - Math.PI / 2.0));
+    // TBD: handle roll command (using this.rot)
 
     this.velocity.mult(this.friction);
     this.position.add(this.velocity);
-    let center = p5.Vector.add(this.position, this.forward);
-    camera(this.position.x, this.position.y, this.position.z, center.x, center.y, center.z, this.up.x, this.up.y, this.up.z);
+    let position = p5.Vector.sub(this.position, p5.Vector.mult(this.right,this.offset[1]));
+    let center = p5.Vector.add(position, this.forward);
+    this.p5.camera(position.x, position.y+this.offset[0], position.z, center.x, center.y+this.offset[0], center.z, this.up.x, this.up.y, this.up.z);
   }
 
   clamp(aNumber, aMin, aMax) {
-    return (aNumber > aMax ? aMax
-        : aNumber < aMin ? aMin
-            : aNumber);
+    return (aNumber > aMax ? aMax :
+      aNumber < aMin ? aMin :
+      aNumber);
   }
+}
+RoverCam.version = "1.1.1";
+// Optional pointerLock applies to all RoverCam instances 
+RoverCam.pointerLock = false;
+// handle exit from pointerLock when user presses ESCAPE
+RoverCam.onPointerlockChange = () => {
+  if (document.pointerLockElement !== RoverCam.canvas &&
+      document.mozPointerLockElement !== RoverCam.canvas) RoverCam.pointerLock = false;
+}
+p5.prototype.createRoverCam = function(){
+  return new RoverCam(this);
 }
